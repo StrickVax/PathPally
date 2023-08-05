903b388c6b9295b75ede3c6f29e64414b67686eb
StrickVax <strickvax@gmail.com>
2023-08-04 19:26:56 -0700

    Add backend

--- server/index.js
+++ server/index.js
@@ -1,16 +1,52 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 const port = 3001;
 
 app.use(express.json());
+app.use(cors());
+
+mongoose.connect('mongodb://localhost/pathpally', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+const itinerarySchema = new mongoose.Schema({
+    destination: String,
+    dates: String,
+    activities: String,
+});
+
+const Itinerary = mongoose.model('Itenerary', itinerarySchema);
 
 app.get('/', (req, res) => {
     res.send('PathPally backend!');
 });
 
+// CRUD endpoints
 app.get('/itinerary', (req, res) => {
-    res.json({ message: 'Itinerary endpoint' });
+    Itinerary.find((err, itineraries) => {
+        if (err) return res.status(500).send(err);
+        res.status(200).json(itineraries);
+    });
 });
+
+
+app.post('/itinerary', (req, res) => {
+    const itinerary = new Itinerary(req.body);
+    itinerary.save((err) => {
+        if (err) return res.status(500).send(err);
+        res.status(200).send('Itinerary created successfully');
+    });
+});
+
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
